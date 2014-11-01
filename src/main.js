b5dbd4d3437cb6b65656ed7b03ac19a6aabc3506
Daniel Buchner <danieljb2@gmail.com>
2014-11-01 08:40:55 -0700

    pushing draft

--- src/main.js
+++ src/main.js
@@ -1,23 +1,44 @@
-(function(){  
-
-  // Create your component here
-  // http://x-tags.org/docs
+(function(){
   
-  xtag.register('x-stub', {
+  var img = document.createElement('img');
+      img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+  
+  xtag.register('x-spinner', {
     lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
-    }, 
-    events: { 
-    
+      created: function() {
+        this.xtag.img = this.appendChild(img.cloneNode(true));
+      }
+    },
+    events: {
+      transitionend: function(e){
+        if (e.target == this.xtag.img && e.propertyName == 'opacity' && !this.hasAttribute('spinning')) {
+          this.removeAttribute('x-spinner-spinning');
+        }
+      }
     },
     accessors: {
-      
+      fade: {
+        attribute: { boolean: true }
+      },
+      spinning: {
+        attribute: { boolean: true }
+      }
     }, 
     methods: {
-      
+      spin: function(duration){
+        this.spinning = true;
+        this.setAttribute('x-spinner-spinning', '');
+        clearTimeout(this.xtag.spinTimer);
+        if (duration) this.xtag.spinTimer = setTimeout(this.stop, duration);
+      },
+      stop: function(){
+        clearTimeout(this.xtag.spinTimer);
+        if (!this.fade) this.removeAttribute('x-spinner-spinning');
+        this.spinning = false; 
+      },
+      toggle: function(duration){
+        this[this.spinning ? 'stop' : 'spin'](duration);
+      }
     }
   });
 
